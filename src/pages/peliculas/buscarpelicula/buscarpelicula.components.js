51e879204634035d21a0f6bb6ba17ac17737e416
Diego Molar de la Cruz <diego.molardlcr@uanl.edu.mx>
2023-05-24 22:45:55 -0600

    Implementacion de buscar pelicula a medias xd

---
+++ src/pages/peliculas/buscarpelicula/buscarpelicula.components.js
@@ -0,0 +1,24 @@
+function ImprimirPeliculas({ id, title, posterPath }) {
+    return `
+      <a href="detallepelicula.html?movieId=${id}&page=1">
+          <div class="card movie-card d-flex align-items-center">
+              <img id="posterPathMovies" src="${posterPath}" class="card-img-top mt-3"
+                  alt="Portada de la película" />
+              <div class="card-body">
+                  <h5 class="card-title text-center">${title}</h5>
+              </div>
+          </div>
+      </a>
+  `;
+  }
+  
+  function Error(message) {
+    return `
+          <h4>ERROR</h4>
+            <p>${message}</p>
+        `;
+  }
+  
+  
+  export { Error, ImprimirPeliculas };
+  
